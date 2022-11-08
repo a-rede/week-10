4505715a6d0366b26400baa2fa245d10dd5e2493
a-rede <abell.rede1@outlook.com>
2022-11-08 10:55:06 -0800

    array and obj example

--- js/example2-array.js
+++ js/example2-array.js
@@ -11,3 +11,21 @@ console.log(fruits)
 //Remove the first element of the array
 fruits.shift()
 console.log(fruits)
+
+// Second Part of the Activity
+let sequence = [1,1,2,3,5,8,13]
+
+for (let element of sequence) {
+    console.log(element)
+}
+
+//Third Part of the Activity
+const person = {
+    name: ['Bob', 'Smith'],
+    age: 32,
+    gender: "male",
+    interests: ['music', 'skiing']
+}
+
+console.log(`My name is ${person.name[0]} ${person.name[1]}`)
+console.log(`I am interested in ${person.interests[0]} ${person.interests[1]}`)
